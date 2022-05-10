2bc01adab7840adc676411eae8484e889a0de372
mark <mddoyne@asu.edu>
2022-05-09 22:52:38 -0600

    commit tree c2e12844736594c49de33b07abc35998258fb243

--- index.js
+++ index.js
@@ -3,3 +3,34 @@ const returnFirstTwoDrivers = (a) =>
     return a.slice(0,2);
 }
 
+const returnLastTwoDrivers = (a) =>
+{
+    return a.slice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (b) =>
+{
+    function fareMultiplier(b)
+    {
+        return (b * 5);
+    };
+
+    return fareMultiplier;
+}
+
+const fareDoubler = (c) =>
+{
+    return c * 2;
+}
+
+const fareTripler = (d) =>
+{
+    return d * 3;
+}
+
+const selectDifferentDrivers = (drivers, returnFirstTwoDrivers) =>
+{
+    return returnFirstTwoDrivers(drivers, returnFirstTwoDrivers);
+}
